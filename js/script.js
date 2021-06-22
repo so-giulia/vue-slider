5290ae4659e57c5fb0d76de27ea5ec687fad4e96
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-22 14:47:19 +0200

    add: slider forwards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const app = new Vue(
     {
-        el: '#root'
+        el: '#root',
+        data:{
+            counter: 0,
+            imgs:[
+                'img/img1.jpeg',
+                'img/img2.jpeg',
+                'img/img3.jpeg',
+                'img/img4.jpeg',
+                'img/img5.jpeg',
+                'img/img6.jpeg'
+            ]
+        },
+        methods:{
+            slideForwards(){
+                (this.counter == this.imgs.length -1) ? this.counter = 0 : this.counter++;
+            }
+        }
     }
 );
